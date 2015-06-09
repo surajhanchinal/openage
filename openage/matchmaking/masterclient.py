6019ba15434770f9c23fb08195901e5c6003a23b
Jonas Jelten <jj@sft.mx>
2015-06-09 14:24:05 +0200

    matchmaking: version check for game client

--- openage/matchmaking/masterclient.py
+++ openage/matchmaking/masterclient.py
@@ -1,6 +1,6 @@
 # Copyright 2015-2015 the openage authors. See copying.md for legal info.
 
-
+import json
 import socket
 
 # master server port = SFTOA
@@ -12,8 +12,22 @@ def test():
 
     s.connect(("localhost", MASTERPORT))
 
-    msg = s.recv(4096)
-    print(msg)
+    serverversion = json.loads(s.recv(4096).decode())
+    print("masterserver version: %s" % (serverversion))
+
+    clientversion = {
+        "peersoftware": "openage",
+        "peertype": "client",
+        "protocol": [0, 0],
+    }
+
+    s.send(b"".join((json.dumps(clientversion).encode(), b"\n")))
+
+    version_handshake = json.loads(s.recv(4096).decode())
+    if version_handshake["compatible"]:
+        print("this client is compatible! congratulations!")
+    else:
+        print("incompatible client, bzzzzzzzzzzt!")
 
 if __name__ == "__main__":
     test()
