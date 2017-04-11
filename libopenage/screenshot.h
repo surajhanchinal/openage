834137222704919d8a736800ec39b7cf0277db94
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-04-11 08:41:07 +0200

    renderer: first working version

--- libopenage/screenshot.h
+++ libopenage/screenshot.h
@@ -5,43 +5,34 @@
 #include <string>
 #include <ctime>
 
+#include <SDL2/SDL.h>
 #include "coord/window.h"
 
 namespace openage {
-namespace renderer {
-class Renderer;
-}
 
 class ScreenshotManager {
 public:
-	ScreenshotManager(renderer::Renderer *renderer);
+	ScreenshotManager();
 	~ScreenshotManager();
 
-	/**
-	 * To be called to save a screenshot.
-	 */
+	/** to be called to save a screenshot */
 	void save_screenshot();
 
+	/** size of the game window, in coord_sdl */
+	coord::window window_size;
+
+
 private:
-	/**
-	 * To be called to get the next screenshot filename into the array.
-	 */
+
+	/** to be called to get the next screenshot filename into the array */
 	std::string gen_next_filename();
 
-	/**
-	 * Contains the number to be in the next screenshot filename.
-	 */
+	/** contains the number to be in the next screenshot filename */
 	unsigned count;
 
-	/**
-	 * Contains the last time when a screenshot was taken.
-	 */
+	/** contains the last time when a screenshot was taken */
 	std::time_t last_time;
 
-	/**
-	 * The renderer where to take screenshots from.
-	 */
-	class renderer::Renderer *renderer;
 };
 
 } // openage
